942910e93f9a44f4e3e5c314e8639410f134b040
fadlylado <111643443+fadlylado@users.noreply.github.com>
2023-11-23 22:21:19 +0700

    secure route

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using spbu.Data;
@@ -6,6 +7,7 @@ using spbu.Models;
 
 namespace spbu.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
